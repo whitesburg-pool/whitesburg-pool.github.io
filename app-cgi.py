358c68ba5812aa50d918550af4916c63a17397d2
Nathan Alderson <nathan.alderson@gmail.com>
2018-04-16 21:09:17 -0500

    Cleaner app API

--- app-cgi.py
+++ app-cgi.py
@@ -7,12 +7,11 @@ import os
 import site
 lib_dir = os.path.join(os.path.dirname(__file__), "python-lib")
 site.addsitedir(lib_dir)
+this_dir = os.path.dirname(__file__)
+site.addsitedir(this_dir)
 
-from flask import Flask
+from app import app
+from wsgiref.handlers import CGIHandler
 
-app = Flask(__name__)
-
-@app.route("/route")
-def hello():
-    return __name__
+CGIHandler().run(app)
 
